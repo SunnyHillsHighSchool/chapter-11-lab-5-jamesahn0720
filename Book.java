5d01bc9c40f446a7181d5662d3e1f338a7956651
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 18:05:22 -0700

    Update Book.java

--- Book.java
+++ Book.java
@@ -3,8 +3,12 @@ public class Book
   
   public static boolean isValidISBN(String isbn)
   {
+    if (isbn.length() == 10)
+      return isValidISBN10(isbn); 
+    else if (isbn.length() == 13)
+      return isValidISBN13(isbn);
     
-    //add your code here
+    return false;
     
     
   }
